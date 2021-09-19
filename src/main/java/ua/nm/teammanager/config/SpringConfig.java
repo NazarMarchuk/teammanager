69021d7bf13da76d94d281d23b6add32043c5af3
NazarMarchuk <nazar.marchuk82@gmail.com>
2021-09-19 12:16:47 +0300

    Changes with database connection in SpringConfig.java

--- src/main/java/ua/nm/teammanager/config/SpringConfig.java
+++ src/main/java/ua/nm/teammanager/config/SpringConfig.java
@@ -60,7 +60,7 @@ public class SpringConfig implements WebMvcConfigurer {
 
         dataSource.setUrl("jdbc:postgresql://localhost:5432/teammanager");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("nazarqwerty1");
+        dataSource.setPassword(""); //enter the password
 
         return dataSource;
     }
